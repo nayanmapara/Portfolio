6b73258b42742bef2a684716fbe78c7f73efa936
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2024-05-25 01:52:32 -0400

    Fixed env

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,9 +31,14 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    const EMAILJS_SERVICE_ID = process.env.EMAILJS_SERVICE_ID || import.meta.env.EMAILJS_SERVICE_ID;
-    const EMAILJS_TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID || import.meta.env.EMAILJS_TEMPLATE_ID;
-    const EMAILJS_PUBLIC_KEY = process.env.EMAILJS_PUBLIC_KEY || import.meta.env.EMAILJS_PUBLIC_KEY;
+    // const EMAILJS_SERVICE_ID = process.env.EMAILJS_SERVICE_ID || import.meta.env.EMAILJS_SERVICE_ID;
+    // const EMAILJS_TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID || import.meta.env.EMAILJS_TEMPLATE_ID;
+    // const EMAILJS_PUBLIC_KEY = process.env.EMAILJS_PUBLIC_KEY || import.meta.env.EMAILJS_PUBLIC_KEY;
+
+    const EMAILJS_SERVICE_ID = process.env.EMAILJS_SERVICE_ID;
+    const EMAILJS_TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID;
+    const EMAILJS_PUBLIC_KEY = process.env.EMAILJS_PUBLIC_KEY;
+
 
     emailjs
       .send(
