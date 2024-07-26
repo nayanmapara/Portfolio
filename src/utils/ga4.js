5b4ec6e1cfc89f71ce42199bb238964d02046820
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2024-07-25 21:03:20 -0400

    Updated env name

--- src/utils/ga4.js
+++ src/utils/ga4.js
@@ -1,6 +1,6 @@
 import ReactGA from 'react-ga4';
 
-const TRACKING_ID = import.meta.env.GA_Tracking_ID;
+const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID;
 ReactGA.initialize(TRACKING_ID);
 
 export const trackPageView = (page) => {
